31e6edbdc80cff697772b538086fb41641a277bc
litecoinmessiah <mp3it.eu@googlemail.com>
2015-04-10 16:37:23 +0100

    Create checkboard.php

---
+++ checkboard.php
@@ -0,0 +1,7 @@
+game_hash=1b78eac17c3a150f7814d86766e4ba721e265761&guess=4&v04=1
+
+
+{"status":"success","guess":4,"outcome":"bitcoins","stake":0.363888,"next":0.015188,"message":"You found <span>13,995 bits<\/span> in tile 4","change":0.013995}
+
+
+
