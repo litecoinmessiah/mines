8a488fb9a071f1bd78f3e0b2b68ba4c322d05957
litecoinmessiah <mp3it.eu@googlemail.com>
2015-04-10 16:44:53 +0100

    Update newgame.php

--- newgame.php
+++ newgame.php
@@ -1,7 +1,7 @@
 <?php
 $rkey = "1-aIcW94fguJ"; //random string hidden from view
-echo hash('sha256', $string);
-echo "<br>Secret hash: ".$string;
+$shash hash('sha256', $rkey);
+echo "<br>Secret hash: ".$shash;
 ?>
 
 //{"status":"success","id":11806534,"game_hash":"1b78eac17c3a150f7814d86766e4ba721e265761","secret":"53dfc081ee31ec8b35f452cd4794e44201057ad863f0bbab58cd6bca4190652f","bet":0.349893,"stake":0.349893,"next":0.013995,"betNumber":"0","gametype":"practice","num_mines":1}
