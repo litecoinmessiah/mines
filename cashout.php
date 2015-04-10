52dc3cbe92a9a3c8741bb34328866a43cb3bb908
litecoinmessiah <mp3it.eu@googlemail.com>
2015-04-10 16:38:39 +0100

    Create cashout.php

---
+++ cashout.php
@@ -0,0 +1,3 @@
+{"status":"success","win":0,"mines":"5","message":"Cashed out 363,888 practice bits.","random_string":"TgeT3iejVP","game_id":"10154256"}
+
+game_hash=1b78eac17c3a150f7814d86766e4ba721e265761
